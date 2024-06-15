d724fa53bb55436bc09758eec7e41b8922f6f9a9
iamnippon <mindbend91@gmail.com>
2024-06-15 14:48:11 +0600

    Fix Bug menu About

--- app/src/main/java/com/zxdeveloper/myapplication/AboutActivity.kt
+++ app/src/main/java/com/zxdeveloper/myapplication/AboutActivity.kt
@@ -1,11 +1,29 @@
 package com.zxdeveloper.myapplication
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.zxdeveloper.myapplication.databinding.ActivityAboutBinding
 
 class AboutActivity: AppCompatActivity() {
+
+    private lateinit var binding: ActivityAboutBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_about)
+        binding = ActivityAboutBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        // back button
+        binding.aboutBackButton.setOnClickListener {
+            finish()
+        }
+        binding.aboutBackButtonHitbox.setOnClickListener {
+            finish()
+        }
+
+
+        }
     }
-}
+
